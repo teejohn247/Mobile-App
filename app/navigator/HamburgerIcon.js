c1930c40ec370d2886d9505bf4f831eb95515781
teejohn247 <teejohn247@gmail.com>
2020-05-13 03:36:48 +0100

    fixed errors and added a select option on the registration page

---
+++ app/navigator/HamburgerIcon.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { withNavigation } from 'react-navigation';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+// import { View, Text } from 'native-base';
+
+
+class HamburgerIcon extends Component{
+    render() {
+    return (
+        <TouchableOpacity
+        style={{
+            width: 44,
+            height: 44,
+            marginLeft: 20,
+            marginTop:30
+        }}
+        onPress={()=>{
+            this.props.navigation.openDrawer();
+        }}>
+            <Icon name='menu' size={20} color='black'/>
+        </TouchableOpacity>
+          
+    )
+    };
+}
+
+export default withNavigation(HamburgerIcon);
--- app/screens/SignUpForm.js
+++ app/screens/SignUpForm.js
@@ -1,54 +1,40 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput,
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, Picker, View, ScrollView, Image, TextInput,
     TouchableWithoutFeedback, Keyboard, SafeAreaView, KeyboardAvoidingView, TouchableOpacity, Dimensions, ImageBackground} from 'react-native';
 import { connect } from "react-redux";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { bindActionCreators } from "redux";
 import PropTypes from "prop-types";
 import { useForm, Controller, } from 'react-hook-form';
-// import { login } from "../redux";
-// import { withStyles } from '@material-ui/core/styles';
-// import { green } from '@material-ui/core/colors';
-// import { FormControlLabel, Radio, Button, FormControl,RadioGroup, Container } from '@material-ui/core'
 import { register } from "../redux/actions";
-import Select from "react-select";
 import { Input } from 'react-native-elements';
 
 
 
 const SignUpForm = ({ register }) => {
-  // const onLogin = (formData) => {
-  //   console.log("Login");
-  //   login(formData);
-  // };
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+    const [selectedValue, setSelectedValue] = useState("");
 
-//   const [selectedValue, setSelectedValue] = React.useState('User');
-
-//   const handleChange = (event) => {
-//     setSelectedValue(event.target.value);
-//   };
-  const { control, handleSubmit, errors } = useForm();
-
-  const onSubmit = async (data, e) => {
-    e.preventDefault();
-    alert(JSON.stringify(data))
-    register(data)
-   }
-
-
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let data = `{"fullname":"${name}","email":"${email}","phone":"${phone}","password":"${password}","role":"${selectedValue}"}`
+        alert(data)
+        register(data)
+       }
+ 
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
         <SafeAreaView>
     <View style={styles.container}>
-        <Controller
-        as={Input}
-        control={control}
-        name="fullname"
-        onChange={args => args[0].nativeEvent.text}
-        rules={{ required: true }}
-        defaultValue=""
+        <Input
         inputContainerStyle={{borderBottomWidth:0}} placeholder="fullname" placeholderTextColor = "#999999"
         containerStyle = {styles.campusInputContainer} style={{fontSize: 2}}
+        onChangeText={text => setName(text)}
+        value={name}
          leftIcon={
             <Icon
               name='person'
@@ -57,19 +43,12 @@ const SignUpForm = ({ register }) => {
               style={{ marginRight: 10 }}
             />
           } />
-          {/* {errors.fullname && <Text>This is required.</Text>} */}
-          {errors.fullname && <Text style={{ color:'red', paddingHorizontal:20 }}>Full Name is required.</Text>}
-
-        <Controller
-            as={Input}
-            control={control}
-            name="email"
-            onChange={args => args[0].nativeEvent.text}
-            rules={{ required: true }}
-            defaultValue=""
-            placeholder="Email"  
+        <Input
+        placeholder="Email"  
         inputContainerStyle={{borderBottomWidth:0}} placeholderTextColor = "#999999"
         containerStyle = {styles.campusInputContainer} 
+        onChangeText={text => setEmail(text)}
+        value={email}
          leftIcon={
             <Icon
               name='mail'
@@ -79,18 +58,12 @@ const SignUpForm = ({ register }) => {
               /> 
          
                }  />
-          {errors.email && <Text style={{ color:'red', paddingHorizontal:20 }}>Email is required.</Text>}
-
-         <Controller
-            as={Input}
-            control={control}
-            name="phone"
-            onChange={args => args[0].nativeEvent.text}
-            rules={{ required: true }}
-            defaultValue=""
+         <Input
             placeholder="Phone Number" 
         inputContainerStyle={{borderBottomWidth:0}} placeholderTextColor = "#999999"
         containerStyle = {styles.campusInputContainer} 
+        onChangeText={text => setPhone(text)}
+        value={phone}
          leftIcon={
             <Icon
               name='phone'
@@ -99,18 +72,12 @@ const SignUpForm = ({ register }) => {
               style={{ marginRight: 10 }}
               /> 
                }  />
-          {errors.phone && <Text style={{ color:'red', paddingHorizontal:20 }}>Phone Number is required.</Text>}
-
-         <Controller
-            as={Input}
-            control={control}
-            name="password"
-            onChange={args => args[0].nativeEvent.text}
-            rules={{ required: true }}
-            defaultValue=""
+         <Input
             placeholder="Password" secureTextEntry={true} 
         inputContainerStyle={{borderBottomWidth:0}} placeholderTextColor = "#999999"
         containerStyle = {styles.campusInputContainer} 
+        onChangeText={text => setPassword(text)}
+        value={password}
          leftIcon={
             <Icon
               name='lock'
@@ -118,66 +85,25 @@ const SignUpForm = ({ register }) => {
               color='#999999'
               style={{ marginRight: 10 }}
               /> 
-         
                }  />
-          {/* <Controller
-            as={Radio}
-            control={control}
-            name="radio"
-            checked={selectedValue === 'user'}
-            onChange={handleChange}
-            inputProps={{ 'aria-label': 'User' }}
-            defaultValue="user"
-           />
-
-          <Controller
-            as={Radio}
-            control={control}
-            name="radio"
-            checked={selectedValue === 'vendor'}
-            value="vendor"
-            onChange={handleChange}
-            inputProps={{ 'aria-label': 'Vendor' }}
-            defaultValue="vendor"
-           /> */}
-           {/* <Radio
-        checked={selectedValue === 'a'}
-        onChange={handleChange}
-        value="a"
-        name="radio-button-demo"
-        inputProps={{ 'aria-label': 'A' }}
-      />
-      <Radio
-        checked={selectedValue === 'b'}
-        onChange={handleChange}
-        value="b"
-        name="radio-button-demo"
-        inputProps={{ 'aria-label': 'B' }}
-      /> */}
-
-{/* <Controller
-        as={<Select options={options} />}
-        control={control}
-        rules={{ required: true }}
-        onChange={([selected]) => {
-          // Place your logic here
-          return selected;
-        }}
-        name="reactSelect"
-        defaultValue={{ value: "chocolate" }}
-      /> */}
-
-          {errors.password && <Text style={{ color:'red', paddingHorizontal:20 }}>Password is required.</Text>}
-        <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
-
+              <View style={styles.campusInputContainer}>
+                <Picker
+        selectedValue={selectedValue}
+        style={{ borderWidth: 10,
+        borderColor: 'white', borderRadius:30, color:'#999999' }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+        <Picker.Item label="Select role" value="" />
+        <Picker.Item label="User" value="user" />
+        <Picker.Item label="Vendor" value="Vendor" />
+      </Picker>
+      </View>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
             <Text style={styles.buttontext}>Register</Text>
         </TouchableOpacity>
-        {/* <Text style={{color:'#fff', marginLeft:'60%',}}>Forgot Password?</Text> */}
 
     </View>
     </SafeAreaView>
     </KeyboardAvoidingView>
-
   );
 }
 
@@ -203,26 +129,26 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     backgroundColor: 'white',
     marginVertical:5,
-    height:50,
-    padding: -20,
+    alignContent:'center',
+    height:45,
+    // padding: -20,
     borderRadius:30,
     shadowColor: 'black',
       shadowOpacity:0.26,
       shadowOffset:{width:0,height:2},
       shadowRadius:8,
-      elevation:3,
+      elevation:7,
       marginBottom:0
   },
   button:{
       backgroundColor: '#3379EC',
       paddingVertical:10,
-      // width: Dimensions.get('window').width - 70,
       paddingLeft:10,
       borderRadius:30 ,
       marginBottom:10,
       marginVertical:5,
       marginTop:'5%',
-      height:50,
+      height:45,
       shadowColor: 'black',
       shadowOpacity:0.26,
       shadowOffset:{width:0,height:2},
